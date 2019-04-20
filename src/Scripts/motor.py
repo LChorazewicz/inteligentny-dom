83651321a27b4152c313bfeadec2912a86f37611
Leszek <leszek.chorazewicz@gmail.com>
2019-04-20 15:59:55 +0200

    poprawki

--- src/Scripts/motor.py
+++ src/Scripts/motor.py
@@ -6,12 +6,6 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(True)
 
-ControlPin = [12, 16, 20, 21]
-
-for pin in ControlPin:
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, 0)
-
 seqForward = [ [1,0,0,0],
         [1,1,0,0],
         [0,1,0,0],
@@ -42,17 +36,21 @@ def main(args):
     else:
         if int(args[1]):
             pin1 = int(args[1])
-            controlPin[0] = pin1
+            ControlPin[0] = pin1
         if int(args[2]):
             pin2 = int(args[2])
-            controlPin[1] = pin2
+            ControlPin[1] = pin2
         if int(args[3]):
             pin3 = int(args[3])
-            controlPin[2] = pin3
+            ControlPin[2] = pin3
         if int(args[4]):
             pin4 = int(args[4])
-            controlPin[3] = pin4
+            ControlPin[3] = pin4
         if pin1 >= 0 and pin2 >= 0 and pin3 >= 0 and pin4 >= 0 and (state == 1 or state == 2):
+            for pin in ControlPin:
+                GPIO.setup(pin, GPIO.OUT)
+                GPIO.output(pin, 0)
+
             if state == 2:
                 for i in range(512):
                     for halfstep in range(8):
@@ -67,11 +65,11 @@ def main(args):
                         for pin in range(4):
                             GPIO.output(ControlPin[pin], seqForward[halfstep][pin])
                             time.sleep(0.001)
-                    GPIO.cleanup()
+                GPIO.cleanup()
 
-         print 1
-      else:
-         print 0
+            print 1
+        else:
+            print 0
 
 if __name__ == "__main__":
    main(sys.argv)
