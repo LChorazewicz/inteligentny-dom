9e0439174c32497178903a90ac832d1afa40e2a7
Leszek <leszek.chorazewicz@gmail.com>
2019-05-26 11:34:10 +0200

    poprawka

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,7 +25,7 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string")
      */
-    private $roles = [];
+    private $roles = '';
 
     /**
      * @var string The hashed password
@@ -65,16 +65,12 @@ class User implements UserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
+        return array_unique(explode(',', $this->roles));
     }
 
     public function setRoles(array $roles): self
     {
-        $this->roles = $roles;
+        $this->roles = implode(',', $roles);
 
         return $this;
     }
